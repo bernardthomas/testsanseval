76916091fcdce3033953fc5ce1b3646c885d48a8
bernardthomas <b.e.r.n.a.r.d@free.fr>
2011-02-15 16:09:58 +0100

    initialisation du projet

---
+++ Controller.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 
+ *  Front Controller de test solution utilisation template sans utiliser eval
+*/
+
+class Controller
+	{
+		/**
+		 * Enter description here ...
+		 */
+		public function __construct()
+		{
+			$this->_run();
+		}
+		
+		/**
+		 * Called by constructor, it's the main controller
+		 */
+		private function _run()
+		{
+			
+			$this->_render();
+		}
+		
+		/**
+		 * render the main layout
+		 */
+		private function _render()
+		{
+			$content = 'contenu';
+			echo file_get_contents('../layout.phtml');
+		}
+	}
+	
